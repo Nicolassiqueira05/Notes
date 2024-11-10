ee11c678dd53a43703474b950c7f4921f22a6088
Nicolas Souza Siqueira <109821803+Nicolassiqueira05@users.noreply.github.com>
2024-11-10 00:19:16 -0300

    back

---
+++ bd/Rascunho.sql
@@ -0,0 +1,34 @@
+create database notas;
+use notas;
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE notas (
+    id SERIAL PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    conteudo JSON,
+    /*Formato do json:
+    texto: 
+    {
+    "titulo": "",
+    "conteudo": ""
+    }
+    lista:
+    {
+    "titulo": "",
+    "conteudo": ["","",""]
+    }
+    */
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualização TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+ALTER TABLE notas
+ADD CONSTRAINT fk_usuario_id
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE;
